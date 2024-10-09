49c5ba85bf893dbb3ea97dd01c15f9d536b873f6
ifigenblat <ifigenblat@gmail.com>
2024-10-09 11:48:28 -0700

    adding new classes and renaming numbers.py to number.py to avoid conflict

--- adventure game.py
+++ adventure game.py
@@ -1,20 +1,24 @@
+import os
 from os import system
-system("clear")
+if os.name == 'nt': 
+	clear = "cls"
+else: 
+	clear = "clear"
 print("Welcome to choose your own Adventure!")
 print("The goal is to find the Python princess...")
 name = input("Enter you name: ")
 name = name.lower()
-system("clear")
+system(clear)
 print("You're standing in front of two doors...")
 print("Do you want the door on the left or right?")
 question = input().lower()
 if question == "left":
-	system("clear")
+	system(clear)
 	print("You fell into a pit and died! GAVE OVER")
 elif question == "right":
-	system("clear")
+	system(clear)
 	print(f"congratulations {name.capitalize()} you found")
 	print("the python princess! YOU WIN!")
 else:
-	system("clear")
+	system(clear)
 	print("Sorry, I don't recognize your response GAME OVER")
---
+++ randomization.py
@@ -0,0 +1,8 @@
+import random
+
+print(random.random())
+
+# defining a range for our random number
+
+our_rand = random.randint(1,10)
+print(our_rand)
